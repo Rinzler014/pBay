e5a893d3357064c898bd434d946f94a05e52f8bf
CanmingCai <caicanming50@gmail.com>
2023-05-21 14:49:26 -0600

    create project, app

---
+++ bayProject/bayApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login(request):
+
+    return render(request, "login.html")
+
+def signup(request):
+
+    return render(request, "signup.html")
+
+def landing(request):
+
+    return render(request, "landing.html")
+
