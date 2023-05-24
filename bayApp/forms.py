62cc691ae50a2531bea4b88436d9c1d913220afc
Jorgergo <102307137+jorgergo02@users.noreply.github.com>
2023-05-23 18:16:24 -0600

    Cambios Frontend Login

--- bayApp/forms.py
+++ bayApp/forms.py
@@ -4,12 +4,12 @@ class LoginForm(forms.Form):
     
     email = forms.EmailField(widget=forms.widgets.TextInput(attrs={
             'class': 'email-field form-control form-control-lg',
-            "placeholder": "Correo Electrónico"}
+            "placeholder": "Email"}
         ))
     
     password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={
                 'class': 'password-field form-control form-control-lg',
-                "placeholder": "Contraseña"}
+                "placeholder": "Password"}
             ))  
     
     
