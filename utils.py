f11582f1193492c418d4afd3606a57a0c0e7cd2d
fodelao29 <a01366363@tec.mx>
2023-06-07 21:52:29 -0600

    comentarios en utils.py y detalles de producto

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pyrebase
+# import decouple
 
 config = {
   "apiKey": "AIzaSyBXTfsPvXFdUkOWFg1H9f6iYYQpAG7U8xs",
@@ -17,3 +18,37 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 storage = firebase.storage()
+
+#The following lines will allow us to acces pbays email
+"""from decouple import config
+
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('pbaymx@gmail.comL')
+EMAIL_HOST_PASSWORD = config('Pbaydelao29')
+EMAIL_USE_TLS = True  """
+
+#We create a message template using smpt when we send an email to any user
+"""
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+
+def send_user_mail(user):
+    subject = 'Disponibilidad de Producto'
+    template = get_template('templates/mi_template_correo.html')
+
+    content = template.render({
+        'user': user,
+    })
+
+    message = EmailMultiAlternatives(subject, #Titulo
+                            ''",
+                                    settings.pbaymx@gmail.com, #Remitente
+                                    [user.email]) #Destinatario
+
+    message.attach_alternative(content, 'text/html')
+    message.send() """ #user.email info will come form the document intrested in our database.
+
+#user = User.objects.last()
+
+#send_user_mail(user)
